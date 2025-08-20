57b8a717f8164ef0c7f2fd14bfd3d83c6316680e
leonardo <leoescuer@protonmail.com>
2025-08-20 20:47:03 +0200

    types for amount and splittarget

--- go_dir/lib.go
+++ go_dir/lib.go
@@ -2,6 +2,19 @@ package main
 
 import "go_dir/cdk_ffi"
 
+// Amount represents a monetary amount with a uint64 value
+type Amount struct {
+	Value uint64
+}
+
+// SplitTarget represents the target for splitting proofs
+type SplitTarget uint
+
+const (
+	SplitTargetNone    SplitTarget = SplitTarget(cdk_ffi.FfiSplitTargetNone)
+	SplitTargetDefault SplitTarget = SplitTarget(cdk_ffi.FfiSplitTargetDefault)
+)
+
 type Wallet struct {
 	wallet cdk_ffi.FfiWalletInterface
 }
@@ -32,7 +45,7 @@ type Unit = cdk_ffi.FfiCurrencyUnit
 
 const Sat Unit = Unit(cdk_ffi.FfiCurrencyUnitSat)
 
-func RestoreFroMMnemonic(minturl string, unit Unit, storage Storage, mnemonic string) (*Wallet, error) {
+func RestoreFromMnemonic(minturl string, unit Unit, storage Storage, mnemonic string) (*Wallet, error) {
 	wallet, err := cdk_ffi.FfiWalletRestoreFromMnemonic(minturl, cdk_ffi.FfiCurrencyUnit(unit), storage.storage, mnemonic)
 	if err != nil {
 		return nil, err
@@ -43,7 +56,6 @@ func RestoreFroMMnemonic(minturl string, unit Unit, storage Storage, mnemonic st
 }
 
 func NewWalletFromMnemonic(minturl string, unit Unit, storage Storage, mnemonic string) (*Wallet, error) {
-
 	wallet, err := cdk_ffi.FfiWalletFromMnemonic(minturl, cdk_ffi.FfiCurrencyUnit(unit), storage.storage, mnemonic)
 	if err != nil {
 		return nil, err
@@ -51,4 +63,68 @@ func NewWalletFromMnemonic(minturl string, unit Unit, storage Storage, mnemonic 
 	return &Wallet{
 		wallet: wallet,
 	}, nil
+}
+
+// Balance returns the wallet's balance
+func (w *Wallet) Balance() (Amount, error) {
+	amount, err := w.wallet.Balance()
+	if err != nil {
+		return Amount{}, err
+	}
+	return Amount{Value: amount.Value}, nil
+}
+
+// GetMintInfo fetches and initializes mint information
+// This should be called after wallet creation to set up the mint in the database
+func (w *Wallet) GetMintInfo() (string, error) {
+	return w.wallet.GetMintInfo()
+}
+
+// Melt executes a melt operation (pay Lightning invoice)
+func (w *Wallet) Melt(quoteId string) (cdk_ffi.FfiMelted, error) {
+	return w.wallet.Melt(quoteId)
+}
+
+// MeltQuote creates a melt quote for paying a Lightning invoice
+func (w *Wallet) MeltQuote(request string) (cdk_ffi.FfiMeltQuote, error) {
+	return w.wallet.MeltQuote(request)
+}
+
+// Mint mints tokens from a quote
+func (w *Wallet) Mint(quoteId string, splitTarget SplitTarget) (Amount, error) {
+	amount, err := w.wallet.Mint(quoteId, cdk_ffi.FfiSplitTarget(splitTarget))
+	if err != nil {
+		return Amount{}, err
+	}
+	return Amount{Value: amount.Value}, nil
+}
+
+// MintQuote creates a mint quote for a specific amount
+func (w *Wallet) MintQuote(amount Amount, description *string) (cdk_ffi.FfiMintQuote, error) {
+	return w.wallet.MintQuote(cdk_ffi.FfiAmount{Value: amount.Value}, description)
+}
+
+// MintQuoteState gets the state of a mint quote
+func (w *Wallet) MintQuoteState(quoteId string) (cdk_ffi.FfiMintQuoteBolt11Response, error) {
+	return w.wallet.MintQuoteState(quoteId)
+}
+
+// MintUrl returns the mint URL
+func (w *Wallet) MintUrl() string {
+	return w.wallet.MintUrl()
+}
+
+// PrepareSend prepares a send operation
+func (w *Wallet) PrepareSend(amount Amount, options cdk_ffi.FfiSendOptions) (cdk_ffi.FfiPreparedSend, error) {
+	return w.wallet.PrepareSend(cdk_ffi.FfiAmount{Value: amount.Value}, options)
+}
+
+// Send sends tokens
+func (w *Wallet) Send(amount Amount, options cdk_ffi.FfiSendOptions, memo *cdk_ffi.FfiSendMemo) (cdk_ffi.FfiToken, error) {
+	return w.wallet.Send(cdk_ffi.FfiAmount{Value: amount.Value}, options, memo)
+}
+
+// Unit returns the wallet's currency unit
+func (w *Wallet) Unit() string {
+	return w.wallet.Unit()
 }
--- go_dir/main.go
+++ go_dir/main.go
@@ -18,7 +18,6 @@ func main() {
 	storage, err := NewStorage()
 	if err != nil {
 		log.Panicf("could not create storage. %+v", err)
-
 	}
 	log.Println("storage: ", storage)
 
@@ -28,12 +27,32 @@ func main() {
 		log.Panicf("could not generate wallet. %+v", err)
 	}
 
-	balance, err := wallet.wallet.Balance()
+	balance, err := wallet.Balance()
+	if err != nil {
+		log.Panicf("could not get balance. %+v", err)
+	}
+	log.Printf("\n current balance. %+v", balance)
+
+	log.Println("trying to get mint quote")
+	mintquote, err := wallet.MintQuote(Amount{Value: 100}, nil)
+	if err != nil {
+		log.Panicf("wallet.MintQuote(Amount{Value: 100}, nil). %+v", err)
+	}
+
+	log.Println("Minting...")
+	amount, err := wallet.Mint(mintquote.Id, SplitTargetDefault)
+	if err != nil {
+		log.Panicf("wallet.Mint(mintquote.Id, SplitTargetDefault). %+v", err)
+	}
+	log.Printf("minted amount: %+v", amount)
+
+	balance, err = wallet.Balance()
 	if err != nil {
 		log.Panicf("could not get balance. %+v", err)
 	}
 
-	log.Printf("\n wallet. %+v", wallet)
-	log.Printf("\n balance. %+v", balance)
+	log.Printf("\n current balance. %+v", balance)
+
+	// wallet.wallet.
 	// balance.Destroy
 }
